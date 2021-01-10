628e8d946ffae4df762cb8d25568dd8f51ea4f4d
github username <joshmrallen@gmail.com>
2021-01-10 16:43:42 -0500

    added ImageViewer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from 'react'
 import Welcome from './Components/Welcome.js'
+import ImageViewer from './Components/ImageViewer.js'
 
 class App extends React.Component {
 
   render(){
     <div>
       <Welcome />
+      <ImageViewer />
     </div>
   }
 }
---
+++ src/Containers/Components/ImageViewer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ImageViewer = () => {
+
+
+    return(
+        <div>
+            <h1>Image Viewer</h1>
+        </div>
+    )
+}
+
+export default ImageViewer
