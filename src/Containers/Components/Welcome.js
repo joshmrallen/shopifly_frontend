fb5ae738e0ab3de4fd82e3e3e57f1080459d6798
github username <joshmrallen@gmail.com>
2021-01-10 16:40:03 -0500

    made App.js a class component and added a Welcome component

---
+++ src/Containers/Components/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Welcome = () => {
+
+
+    return(
+        <div>
+            <h1>Welcome to Shopifly!</h1>
+            <p>An Image Hoarding App</p>
+        </div>
+
+    )
+}
+
+export default Welcome
