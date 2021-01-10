7f89285e3ea5158f1ff3690779acbf2e0fa9baf9
github username <joshmrallen@gmail.com>
2021-01-10 16:49:05 -0500

    added UploadImage container and UploadForm component

---
+++ src/Components/UploadForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UploadForm = () => {
+
+    return(
+        <div>
+            <h1>Upload Form/Button for Images Goes Here</h1>
+        </div>
+    )
+}
+
+export default UploadForm
---
+++ src/Containers/UploadImage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import UploadForm from '../Components/UploadForm'
+
+const UploadImage = () => {
+    return(
+        <div>
+            <h1>Uploader Container</h1>
+            <UploadForm />
+        </div>
+    )
+}
+
+export default UploadImage
